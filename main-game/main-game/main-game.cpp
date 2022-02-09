51e64083edd029ea3daaa597f8f87a7ca983f904
Yuki Dimanov <iynazarenko19@codingburgas.bg>
2022-02-09 18:25:30 +0200

    Add programm functions

---
+++ main-game/main-game/main-game.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<iomanip>
+#include<conio.h>
+#include<stdlib.h>
+#include <string>
+#include <windows.h>
+
+
+using namespace std;
+
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define RED
+
+void ClearArea() {
+    system("CLS");
+}
+
+int main() {
+
+}
